acf211b75c60da2398c2f16cefe9c92316a13a9a
Pedro <pedrohenriquebritooliveira@gmail.com>
2022-10-04 23:43:39 -0300

    :lipstick: feat: Add global style

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Sign from "./pages/Sign";
+import GlobalStyle from "./styles/globalStyles";
 
 function App() {
   return (
     <BrowserRouter>
+      <GlobalStyle />
       <Routes>
         <Route path="/" element={<Sign />} />
       </Routes>
---
+++ src/styles/globalStyles.js
@@ -0,0 +1,72 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  /* Reset CSS */
+  html, body, div, span, applet, object, iframe,
+  h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+  a, abbr, acronym, address, big, cite, code,
+  del, dfn, em, img, ins, kbd, q, s, samp,
+  small, strike, strong, sub, sup, tt, var,
+  b, u, i, center,
+  dl, dt, dd, ol, ul, li,
+  fieldset, form, label, legend,
+  table, caption, tbody, tfoot, thead, tr, th, td,
+  article, aside, canvas, details, embed, 
+  figure, figcaption, footer, header, hgroup, 
+  menu, nav, output, ruby, section, summary,
+  time, mark, audio, video {
+    margin: 0;
+    padding: 0;
+    border: 0;
+    font-size: 100%;
+    font: inherit;
+    vertical-align: baseline;
+  }
+  /* HTML5 display-role reset for older browsers */
+  article, aside, details, figcaption, figure, 
+  footer, header, hgroup, menu, nav, section {
+    display: block;
+  }
+  body {
+    line-height: 1;
+  }
+  ol, ul {
+    list-style: none;
+  }
+  blockquote, q {
+    quotes: none;
+  }
+  blockquote:before, blockquote:after,
+  q:before, q:after {
+    content: '';
+    content: none;
+  }
+  table {
+    border-collapse: collapse;
+    border-spacing: 0;
+  }
+  /* Estilos Personalizados */
+  *{
+    font-family: 'Lexend Deca', sans-serif;
+    --cor-cinza: #9C9C9C;
+    --cor-preta: #000000;
+    --cor-branca: #FFFFFF;
+    --cor-verde: #5D9040;
+    --cor-verde-claro: #80CC74;
+    --cor-verde-scroll: #78B159;
+  }
+  
+  body{
+    display: flex;
+    justify-content: center;
+    min-height: 100vh;
+    height: 100%;
+    font-size: 16px;
+  }
+  
+  #root{
+    width: 100%;
+  }
+`;
+
+export default GlobalStyle;
