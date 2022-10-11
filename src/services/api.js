9b51fe86c92f36dea58163ed687b7bc41cbabad0
Pedro <pedrohenriquebritooliveira@gmail.com>
2022-10-10 23:52:03 -0300

    :sparkles: API instance

--- src/services/api.js
+++ src/services/api.js
@@ -1,1 +1,7 @@
-export const URL = process.env.REACT_APP_URL;
+import axios from "axios";
+
+const instance = axios.create({
+  baseURL: process.env.REACT_APP_API_BASE_URL
+});
+
+export default instance;
