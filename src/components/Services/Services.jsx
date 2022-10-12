f1f53357a6c0c00188e64957f6a315e33015b0e4
Pedro <pedrohenriquebritooliveira@gmail.com>
2022-10-12 20:20:00 -0300

    :sparkles: feat: Services modal

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -2,39 +2,51 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { Container, Service } from "./ServicesStyle";
 import getServices from "../../services/getServices";
+import { LoadingGif } from "../../styles/loadingGif";
+import loadingGif from "../../images/loadingGif.gif";
 
 export default function Services() {
   const navigate = useNavigate();
   const [services, setServices] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   function gotToServicePage(serviceId) {
     navigate(`/service`);
   }
 
   useEffect(() => {
-    const promise = getServices();
-
-    promise
-      .then((res) => {
-        setServices(res.data);
-        console.log(services);
-      })
-      .catch((error) => {
+    async function getData() {
+      try {
+        const promise = await getServices();
+        setServices(promise.data);
+        setLoading(false);
+      } catch (error) {
         alert(
-          `Erro ao carregar serviços: \n\n${error.response.status} - ${error.response.data}`
+          `Erro ao carregar profissionais: \n\n${error.response.status} - ${error.response.data}`
         );
-      });
+      }
+    }
+    getData();
   }, []);
 
   return (
     <Container>
-      {services.map((service, key) => {
-        return (
-          <Service key={key} onClick={() => navigate(`/service/${service.id}`)}>
-            {service.service}
-          </Service>
-        );
-      })}
+      {loading ? (
+        <LoadingGif>
+          <img src={loadingGif} alt="" />
+        </LoadingGif>
+      ) : (
+        services.map((service, key) => {
+          return (
+            <Service
+              key={key}
+              onClick={() => navigate(`/service/${service.id}`)}
+            >
+              {service.service}
+            </Service>
+          );
+        })
+      )}
     </Container>
   );
 }
